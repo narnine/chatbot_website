045c30b166ac595e9ec184f42aae0679465d0a0a
narnine <nariman_naazarov@mail.ru>
2018-04-20 15:29:45 +0600

    upgrade

--- ChatBotProject/settings.py
+++ ChatBotProject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/chatbot_website/'
+STATIC_URL = '/static/chatbot_website/css/style/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
--- chatbot_website/urls.py
+++ chatbot_website/urls.py
@@ -4,7 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('begin/$', views.begin, name='begin'),
-    path('ourtime/$', views.ourtime, name='ourtime')
+    path(r'', views.index, name='index'),
+    path(r'begin', views.begin, name='begin'),
+    path(r'ourtime', views.ourtime, name='ourtime'),
+    path(r'future', views.future, name='future'),
+    path(r'simple', views.simple, name='simple'),
+    path(r'hard', views.hard, name='hard'),
+    path(r'example', views.example, name='example'),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
