9f73cdc1debc24b8ddef5f220adeb30feb800cfd
Нариман Назаров <nariman.nazarov9@gmail.com>
2021-03-23 17:04:19 +0600

    added myself register for sidebar

--- chatbot_website/views.py
+++ chatbot_website/views.py
@@ -49,21 +49,17 @@ def example(request):
 
 def blog(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categories': categories
     }
     return render(request, template_name='chatbot_website/blog.html', context=context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category,
     }
     return render(request, template_name='chatbot_website/category.html', context=context)
